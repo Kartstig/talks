1c5b25f1a8c4059c4518d23ecb2fe5b3c53768a6
Herman Singh <kartstig@gmail.com>
2018-03-04 16:27:05 -0500

    Add API for calling get/put

--- 2018/0306-Genserver_vs_Agent/kvgs/src/kvgs.erl
+++ 2018/0306-Genserver_vs_Agent/kvgs/src/kvgs.erl
@@ -1,19 +1,31 @@
--module(kvgs_app).
+-module(kvgs).
 
 -behaviour(gen_server).
 
 -export([start/2, start_link/0]).
 -export([code_change/3, init/1, handle_call/3, handle_info/2, terminate/2]).
+-export([get/2, put/2]).
 
+-define(PROC, kvstore).
 
 start(normal, []) ->
-  kvgs_app:start_link().
+  kvgs:start_link().
 
 start_link() ->
-  gen_server:start_link({local, kvstore}, ?MODULE, [], []).
+  gen_server:start_link({local, ?PROC}, ?MODULE, [], []).
 
 init([]) ->
   {ok, #{}}.
+
+get(Key, Default) when is_atom(Key) ->
+  gen_server:call(?PROC, {get, Key, Default});
+get(Key, Default) ->
+  gen_server:call(?PROC, {get, list_to_atom(Key), Default}).
+
+put(Key, Value) when is_atom(Key) ->
+  gen_server:call(?PROC, {put, Key, Value});
+put(Key, Value) ->
+  gen_server:call(?PROC, {put, list_to_atom(Key), Value}).
 
 handle_call({get, Key}, _From, State) ->
   Val = maps:get(Key, State),
